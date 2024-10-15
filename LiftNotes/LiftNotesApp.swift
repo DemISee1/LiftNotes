151e55a6f6c77a90dc77f426d22de6f4f123edb2
DemISee1 <118212127+DemISee1@users.noreply.github.com>
2024-10-15 10:28:32 -0500

    Initial Commit

---
+++ LiftNotes/LiftNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftNotesApp.swift
+//  LiftNotes
+//
+//  Created by Demessie Amede on 2024-10-15.
+//
+
+import SwiftUI
+
+@main
+struct LiftNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiftNotesTests/LiftNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiftNotesTests.swift
+//  LiftNotesTests
+//
+//  Created by Demessie Amede on 2024-10-15.
+//
+
+import Testing
+@testable import LiftNotes
+
+struct LiftNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
